404abe2dc7fb93afa03fbebe484976c3231a30b7
deepak <deepakkumardash313@gmail.com>
2022-02-27 00:31:26 +0530

    update bookmarks component

--- src/screens/Bookmarks.screen.tsx
+++ src/screens/Bookmarks.screen.tsx
@@ -1,11 +1,63 @@
-import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import {gql, useQuery} from 'urql';
+import Story from '../components/Story';
+import {StorySummaryFields} from '../graphql/fragments';
+import {
+  AllBookmarksQuery,
+  AllBookmarksQueryVariables,
+} from '../graphql/__generated__/operationTypes';
+
+const BOOKMARKS_QUERY = gql`
+  query AllBookmarks {
+    bookmarks {
+      id
+      story {
+        ...StorySummaryFields
+      }
+    }
+  }
+
+  ${StorySummaryFields}
+`;
 
 const BookmarksScreen: React.FC = () => {
+  const [{data, fetching, error}] = useQuery<
+    AllBookmarksQuery,
+    AllBookmarksQueryVariables
+  >({query: BOOKMARKS_QUERY});
+
+  if (fetching) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator color={'grey'} />
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.error}>Something went wrong {error.message}</Text>
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Bookmarks.screen</Text>
-    </View>
+    <FlatList
+      contentContainerStyle={styles.flatlistContainer}
+      style={styles.flatlist}
+      data={data?.bookmarks}
+      keyExtractor={item => item.id}
+      ItemSeparatorComponent={() => <View style={styles.separator} />}
+      renderItem={({item}) => <Story item={item.story} />}
+    />
   );
 };
 
@@ -13,8 +65,26 @@ export default BookmarksScreen;
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    flex: 1,
+  },
+
+  flatlistContainer: {
+    paddingVertical: 30,
+  },
+
+  separator: {
+    height: 1,
+    backgroundColor: 'black',
+    marginVertical: 40,
+  },
+
+  flatlist: {
+    paddingHorizontal: 20,
+  },
+
+  error: {
+    color: 'red',
   },
 });
